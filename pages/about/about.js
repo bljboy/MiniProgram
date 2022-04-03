c263707c591d41d1432eb6bb665e475918ae283c
bljboy <bljboy55@gmail.com>
2022-04-03 14:44:30 +0800

    网络API

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,12 @@
-// pages/about/about.js
+import request from "../../service/network.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleNetWork(){
+    request({
+      url:"https://api.jisuapi.com/news/get?channel=%E5%A4%B4%E6%9D%A1&start=0&num=10&appkey=28484dd6dafeebd7",
+    }).then(res=>{
+      console.log(res);
+    }).catch(err =>{
+      console.log(err);
+    })
   }
 })
